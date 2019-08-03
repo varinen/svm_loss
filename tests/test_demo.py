548364ca9bdcb397552c3180667dd8cd182efcfd
Oleg Ishenko <oleg.ishenko@divisionlab.com>
2019-08-03 11:39:07 +0200

    SL-01 Testing the index route

--- tests/test_demo.py
+++ tests/test_demo.py
@@ -1,2 +1,11 @@
 """Test suite for the Demo package."""
 
+from flask import url_for
+
+
+def test_demo_index(app, client):
+    """Test the response from the index page."""
+    with app.test_request_context():
+        resp = client.get(url_for('demo.index'))
+
+        assert 'Linear Classifier Demo' in str(resp.data)

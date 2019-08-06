a6dccdce916bc425db900de15956d7e61f8739a0
Oleg Ishenko <oleg.ishenko@divisionlab.com>
2019-08-06 07:54:33 +0200

    SL-03 Tests for the get_data route

--- tests/test_demo.py
+++ tests/test_demo.py
@@ -12,17 +12,40 @@ def test_demo_index(app, client):
 
 
 def test_init_params(app, client):
-    """Test the response from the init_params route."""
+    """Test the response from the init_params and random_params routes."""
+    with app.app_context():
+        urls = [
+            url_for('demo.init_params'),
+            url_for('demo.random_params')
+        ]
     with app.test_request_context():
-        resp = client.get(url_for('demo.init_params'))
-        assert resp.content_type == 'application/json'
+        for url in urls:
+            resp = client.get(url)
+            assert resp.content_type == 'application/json'
 
-        data = json.loads(resp.data)
-        assert isinstance(data, dict)
-        assert data['biases']
-        assert data['weights']
-        assert len(data['biases']) == 3
-        assert len(data['weights']) == 3
-        assert len(data['weights'][0]) == 2
-        assert len(data['weights'][1]) == 2
-        assert len(data['weights'][2]) == 2
+            data = json.loads(resp.data)
+            assert isinstance(data, dict)
+            assert data['biases']
+            assert data['weights']
+            assert len(data['biases']) == 3
+            assert len(data['weights']) == 3
+            assert len(data['weights'][0]) == 2
+            assert len(data['weights'][1]) == 2
+            assert len(data['weights'][2]) == 2
+
+
+def test_get_data_params(app, client):
+    """Test the data generation with the get_data route."""
+    with app.app_context():
+        urls = [
+            url_for('demo.get_data'),
+            url_for('demo.get_data', rand=1)
+        ]
+    with app.test_request_context():
+        for url in urls:
+            resp = client.get(url)
+            assert resp.content_type == 'application/json'
+            data = json.loads(resp.data)
+            assert isinstance(data, list)
+            assert len(data) == 9
+            assert len(data[0]) == 3

f361f069d4668b84cb63d0d9e64bb5289032d626
Oleg Ishenko <oleg.ishenko@divisionlab.com>
2019-08-09 08:35:52 +0200

    SL-03 Plot generating functionality

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,6 +1,7 @@
 """Tests for the utils module."""
 
-from app.demo.utils import get_urls, init_params, init_data
+import numpy as np
+from app.demo.utils import *
 
 
 def test_get_urls(app):
@@ -38,7 +39,7 @@ def test_init_params(app):
         assert len(result['biases']) == 3
 
 
-def test_init_data(app):
+def test_init_data():
     """Test the data generation by the init_data function."""
     rands = [True, False]
 
@@ -49,3 +50,65 @@ def test_init_data(app):
         assert len(result) == 9
         for i in result:
             assert len(i) == 3
+
+
+def test_model_scores():
+    """Test the generation of class scores."""
+    params = np.array([[1, 1, 1], [2, 2, 2]])
+    data = np.array([[3, 3], [4, 4]])
+
+    scores = model_scores(data, params)
+    assert scores.shape == (2, 2)
+    assert scores[0][0] == 7
+    assert scores[0][1] == 14
+    assert scores[1][0] == 9
+    assert scores[1][1] == 18
+
+
+def test_model():
+    """Test the class prediction based on the highest score."""
+    params = np.array([[1, 1, 1], [2, 2, 2]])
+    data = np.array([[3, 3], [4, 4]])
+
+    preds = model(data, params)
+    assert preds.shape == (2,)
+    assert preds[0] == 1
+    assert preds[1] == 1
+
+
+def test_get_perp():
+    """Test the generation of parameters of a perpendicular to a line defined
+    by params."""
+    params = np.array([-4, -1, 10])
+    x_min = 0
+    x_max = 14
+
+    x_mid, y_mid, dx, dy = get_perp(x_min, x_max, params)
+
+    assert x_mid == 7
+    assert y_mid == -18
+    assert dx == 0.5
+    assert dy == 0.125
+
+
+def test_prepare_data():
+    """Test the generation of meshgrid coordinates."""
+    data = np.array([[3, 3, 1], [4, 4, 2]])
+    xx, yy, x_min, x_max, y_min, y_max, x_, y_ = prepare_data(data, 0.005)
+    assert xx.shape == (600, 600)
+    assert yy.shape == (600, 600)
+    assert x_max == 5
+    assert x_min == 2
+    assert y_min == 2
+    assert y_max == 5
+    assert x_.shape == (2, 2)
+    assert y_.shape == (2,)
+
+
+def test_generate_plot_image_string():
+    """Generate the ability to produce a string representing plotted data."""
+    data = np.array([[3, 3, 1], [4, 4, 2]])
+    params = np.array([[1, 1, 1], [2, 2, 2]])
+
+    str_ = generate_plot_image_string(data, params)
+    assert len(str_) > 0

0c34a4393b7dc628d5c72f336d2a0032b431909b
Oleg Ishenko <oleg.ishenko@divisionlab.com>
2019-11-05 15:34:55 +0100

    feat: added test infrastructure

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -4,7 +4,7 @@ class App extends Component {
       render() {
         return (
             <div>
-                <h1>SVM Loss Demo</h1>
+                <h1>SVM Loss Demo!</h1>
             </div>
         );
     }

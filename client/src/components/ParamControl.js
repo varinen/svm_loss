7399a5f94697b256c67ca447129fb049601cca17
Oleg Ishenko <oleg.ishenko@divisionlab.com>
2019-11-06 18:43:20 +0100

    test: check ParamsCOntrol for the correct number of rows

--- client/src/components/ParamControl.js
+++ client/src/components/ParamControl.js
@@ -3,13 +3,14 @@ import {connect} from 'react-redux';
 
 const renderWeight = (weightItem, num) => (
     weightItem.map((weight, index) => (
-            <div className="col-xs-4 p-1">
+            <div key={`weight-${index}`} className="col-xs-4 p-1">
                 <div className="form-group">
                     <label className="small">W[{num},{index}]</label>
                     <input
                         type="number"
                         step="1"
                         className="form-control form-control-sm"
+                        onChange={() => {}}
                         value={weight}/>
                     <span
                         className="d-block text-red font-italic small">0</span>
@@ -24,13 +25,14 @@ export const ParamControl = ({params}) => {
         return (
             params.weights.map((weight, index) => {
                 return (
-                    <div className="row">
+                    <div key={`param-row-${index}`} className="row">
                         {renderWeight(weight, index)}
-                        <div className="col-xs-4 p-1">
+                        <div key={`bias-${index}`} className="col-xs-4 p-1">
                             <div className="form-group">
                                 <label className="small">b[{index}]</label>
                                 <input type="number"
                                        step="1"
+                                       onChange={() => {}}
                                        value={params.biases[index]}
                                        className="form-control form-control-sm"/>
                                 <span
@@ -43,7 +45,7 @@ export const ParamControl = ({params}) => {
         )
     }
 
-    return null;
+    return <div></div>;
 };
 
 export default connect(state => {
--- client/src/components/ParamControl.test.js
+++ client/src/components/ParamControl.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {shallow} from 'enzyme';
+import {shallow, mount} from 'enzyme';
 import {ParamControl} from './ParamControl';
 import configureMockStore from "redux-mock-store";
 
@@ -7,10 +7,29 @@ const mockStore = configureMockStore();
 const store = mockStore({});
 
 describe('ParamControl', () => {
-    const control = shallow(<ParamControl store={store}/>);
+    let props = {
+        store: store
+    };
+    let control = mount(<ParamControl store={props.store}/>);
 
     it('renders correctly', () => {
         expect(control).toMatchSnapshot();
     });
+
+    describe('displays params correctly', () => {
+        props = {
+            params: {
+                biases: [-0.04, 0.45, 0.41],
+                weights: [[-2.05, -4.55], [-2.85, -4.34], [-0.87, 2.87]]
+            }
+        };
+        control = mount(<ParamControl params={props.params}/>);
+
+        it('generates correct number of rows', () => {
+            expect(control.find('.row').length).toEqual(3);
+        })
+
+    })
+
 });
 

30caddac45fcc87cb4211bcc85a03cee6cf4fa72
Oleg Ishenko <oleg.ishenko@divisionlab.com>
2019-12-09 16:28:13 +0100

    refactor: rewrote the ParamControl component

--- client/src/components/ParamControl.js
+++ client/src/components/ParamControl.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Component} from 'react';
 import {connect} from 'react-redux';
 
 const renderWeight = (weightItem, num) => (
@@ -10,7 +10,8 @@ const renderWeight = (weightItem, num) => (
                         type="number"
                         step="1"
                         className="form-control form-control-sm"
-                        onChange={() => {}}
+                        onChange={() => {
+                        }}
                         value={weight}/>
                     <span
                         className="d-block text-red font-italic small">0</span>
@@ -20,33 +21,42 @@ const renderWeight = (weightItem, num) => (
     )
 );
 
-export const ParamControl = ({params}) => {
-    if (params && params.weights && params.biases) {
-        return (
-            params.weights.map((weight, index) => {
-                return (
-                    <div key={`param-row-${index}`} className="row">
-                        {renderWeight(weight, index)}
-                        <div key={`bias-${index}`} className="col-xs-4 p-1">
-                            <div className="form-group">
-                                <label className="small">b[{index}]</label>
-                                <input type="number"
-                                       step="1"
-                                       onChange={() => {}}
-                                       value={params.biases[index]}
-                                       className="form-control form-control-sm"/>
-                                <span
-                                    className="d-block text-red font-italic small">0</span>
+
+export class ParamControl extends Component {
+
+    render() {
+        const params = this.props.params;
+
+        if (params && params.weights && params.biases) {
+            console.log(params);
+            return (
+                params.weights.map((weight, index) => {
+                    return (
+                        <div key={`param-row-${index}`} className="row">
+                            {renderWeight(weight, index)}
+                            <div key={`bias-${index}`}
+                                 className="col-xs-4 p-1">
+                                <div className="form-group">
+                                    <label className="small">b[{index}]</label>
+                                    <input type="number"
+                                           step="1"
+                                           onChange={() => {
+                                           }}
+                                           value={params.biases[index]}
+                                           className="form-control form-control-sm"/>
+                                    <span
+                                        className="d-block text-red font-italic small">0</span>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                )
-            })
-        )
-    }
+                    )
+                })
+            )
+        }
 
-    return <div></div>;
-};
+        return <div></div>;
+    }
+}
 
 const mapPropsToStore = state => {
     const {params} = state;

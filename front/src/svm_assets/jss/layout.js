d2d1222855b5ede2a23b043364b7dd39558869e9
OIeg Ishenko <oleg.ishenko@divisionlab.com>
2019-12-28 11:00:51 +0100

    feat: custom svm_demo assets

---
+++ front/src/svm_assets/jss/layout.js
@@ -0,0 +1,22 @@
+const pt1 = {
+    paddingTop: "1rem"
+};
+
+const pt2 = {
+    paddingTop: "2rem"
+};
+
+const pt3 = {
+    paddingTop: "3rem"
+};
+
+const pt4 = {
+    paddingTop: "4rem"
+};
+
+export {
+    pt1,
+    pt2,
+    pt3,
+    pt4
+}
---
+++ front/src/svm_assets/jss/views/demo.js
@@ -0,0 +1,50 @@
+import {container, title} from "assets/jss/material-kit-react.js";
+import {pt1, pt2, pt3, pt4} from "../layout";
+
+const demoStyles = {
+    pt1: {
+        ...pt1
+    },
+    pt2: {
+        ...pt2
+    },
+    pt3: {
+        ...pt3
+    },
+    pt4: {
+        ...pt4
+    },
+    container: {
+        zIndex: "12",
+        color: "#FFFFFF",
+        ...container
+    },
+    title: {
+        ...title,
+        display: "inline-block",
+        position: "relative",
+        marginTop: "30px",
+        minHeight: "32px",
+        color: "#FFFFFF",
+        textDecoration: "none"
+    },
+    subtitle: {
+        fontSize: "1rem",
+        maxWidth: "500px",
+        margin: "10px auto 0",
+        fontWeight: 200
+    },
+    main: {
+        background: "#FFFFFF",
+        position: "relative",
+        zIndex: "3"
+    },
+    mainRaised: {
+        margin: "-60px 30px 0px",
+        borderRadius: "6px",
+        boxShadow:
+            "0 16px 24px 2px rgba(0, 0, 0, 0.14), 0 6px 30px 5px rgba(0, 0, 0, 0.12), 0 8px 10px -5px rgba(0, 0, 0, 0.2)"
+    },
+};
+
+export default demoStyles;

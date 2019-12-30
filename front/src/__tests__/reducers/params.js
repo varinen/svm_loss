40a2560e794ad4d7bf73818e19b8711c86472eff
OIeg Ishenko <oleg.ishenko@divisionlab.com>
2019-12-30 13:24:21 +0100

    test: updateParams tests

--- front/src/__tests__/reducers/params.js
+++ front/src/__tests__/reducers/params.js
@@ -6,6 +6,8 @@ import {fetchParams} from "../../actions";
 import {apiUrl, pathGetParams} from "../../api";
 import paramsReducer from "../../reducers/params";
 
+import {updateParams} from "../../actions";
+
 
 const middlewares = [thunk];
 let store;
@@ -14,14 +16,13 @@ beforeEach(() => {
     store = createStore(paramsReducer, applyMiddleware(thunk))
 });
 
-
 test('initial state has to have a params object with empty weights and biases', () => {
-    expect (store.getState()).toEqual({weights:[], biases:[]});
+    expect(store.getState()).toEqual({weights: [], biases: []});
 });
 
 test('fetchParams action should populate weights and biases', () => {
     const rand = 0;
-    const paramsObjs = {weights:[[1,2],[3,4]], biases:[1,2]};
+    const paramsObjs = {weights: [[1, 2], [3, 4]], biases: [1, 2]};
 
     nock(`${apiUrl}`)
         .get(`${pathGetParams}?rand=${rand}`)
@@ -33,5 +34,20 @@ test('fetchParams action should populate weights and biases', () => {
         .then(() => {
             expect(store.getState()).toEqual(paramsObjs);
         });
+});
 
+test('updateParams can update a weight entry', () => {
+    const initState = {weights: [[1, 2], [3, 4]], biases: [1, 2]};
+    const expectedState = {weights: [[1, 2], [3, 10]], biases: [1, 2]};
+    const action = updateParams(1, 1, 'weights', 10);
+
+    expect(paramsReducer(initState, action)).toEqual(expectedState);
+});
+
+test('updateParams can update a bias entry', () => {
+    const initState = {weights: [[1, 2], [3, 4]], biases: [1, 2]};
+    const expectedState = {weights: [[1, 2], [3, 4]], biases: [1, 10]};
+    const action = updateParams(1, 1, 'biases', 10);
+
+    expect(paramsReducer(initState, action)).toEqual(expectedState);
 });

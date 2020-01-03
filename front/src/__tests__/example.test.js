e1a80806a16f269cb907a3e2e9474e9d70459a91
OIeg Ishenko <oleg.ishenko@divisionlab.com>
2020-01-03 10:09:03 +0100

    refactor: removed unused files

--- front/src/__tests__/example.test.js
+++
@@ -1,9 +0,0 @@
-test('example test', () =>
-  expect(1 + 1).toBe(2)
-)
-
-test('object example', () => {
-  const data = { hello: 'world' }
-  data['hi'] = 'world'
-  expect(data).toEqual({ hello: 'world', hi: 'world' })
-})
